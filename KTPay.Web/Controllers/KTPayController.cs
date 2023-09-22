e4a30a8e40e2444fa8ef62c1d6f4ff46651e8e0c
Osman Ozen <osmanozen@outlook.com>
2023-09-22 09:59:54 +0300

    let's start up

---
+++ KTPay.Web/Controllers/KTPayController.cs
@@ -0,0 +1,392 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using KTPay.Models;
+using KTPay.Models.Request;
+using KTPay.Models.Response;
+using KTPay.Models.Response.Generic;
+using KTPay.Services.Interfaces;
+using KTPay.Web.Helpers;
+using KTPay.Web.Models;
+
+namespace KTPay.Web.Controllers {
+    
+    public class KTPayController : Controller {
+        
+        private readonly ILogger<KTPayController> _logger;
+        private readonly IConfiguration _configuration;
+        private readonly IKTPayService _ktPayService;
+
+        public KTPayController(ILogger<KTPayController> logger, IConfiguration configuration, IKTPayService ktPayService) {
+            _logger = logger;
+            _configuration = configuration;
+            _ktPayService = ktPayService;
+        }
+
+        [HttpGet]
+        public IActionResult Index() {
+            
+            return View();
+        }
+
+        /// <summary>
+        /// 3D Payment
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IActionResult> Payment() {
+            
+            var envConfig = _configuration.GetSection("KTPayConfig:"+KTPayConfig.TEST).Get<KTPayConfig>() ?? new KTPayConfig();
+            var cardConfig = _configuration.GetSection("CardConfig:"+CardConfig.VPOSTest).Get<CardConfig>() ?? new CardConfig();
+
+            var request = new PaymentRequest() {
+                PaymentType = 1,
+                Language = Language.TR.GetValue(),
+                MerchantOrderId = "KT TEST",
+                SuccessUrl = "http://localhost:3000/KTPay/Success",
+                FailUrl = "http://localhost:3000/KTPay/Fail",
+                MerchantId = Convert.ToInt32(envConfig.MerchantId),
+                CustomerId = Convert.ToInt32(envConfig.CustomerId),
+                Username = envConfig.Username,
+                Amount = "100",
+                Currency = Currency.TRY.ToString(),
+                InstallmentCount = 1,
+                Customer = new Customer() {
+                    FullName = "JOHN DOE",
+                    IdentityNumber = "12345678901",
+                    Email = "noreply@kuveytturk.com.tr",
+                    PhoneNumber = new Phone() {
+                        Cc = "90",
+                        Subscriber = "5001112233"
+                    },
+                    IpAddress = "127.0.0.1"
+                },
+                Card = new Card() {
+                    CardHolderName = cardConfig.CardHolderName,
+                    CardNumber = cardConfig.CardNumber,
+                    ExpireMonth = cardConfig.ExpireMonth,
+                    ExpireYear = cardConfig.ExpireYear,
+                    SecurityCode = cardConfig.SecurityCode 
+                },
+                Cart = new List<CartItem>() {
+                    new CartItem() {
+                        CartItemName = "Product PHYSICAL",
+                        CartItemUrl = "https://www.kuveytturk.com.tr",
+                        CartItemType = (int)CartItemType.PHYSICAL,
+                        Quantity = 1.5,
+                        Price = "10",
+                        TotalAmount = "15"
+                    },
+                    new CartItem() {
+                        CartItemName = "Product VIRTUAL",
+                        CartItemUrl = "https://www.kuveytturk.com.tr",
+                        CartItemType = (int)CartItemType.VIRTUAL,
+                        Quantity = 1,
+                        Price = "85",
+                        TotalAmount = "85"
+                    }
+                },
+                InvoiceAddress = new InvoiceAddress() {
+                    Company = "XXX LTD.STI.",
+                    TaxNumber = "111111111",
+                    TaxOffice = "XXXXXXXXX",
+                    Address = "ADDRESS",
+                    State = "IZMIT",
+                    City = City.Kocaeli.ToString(),
+                    Country = Country.Turkiye.ToString(),
+                    ZipCode = "123456"
+                },
+                ShippingAddress = new ShippingAddress() {
+                    FullName = "JOHN DOE",
+                    Address = "ADDRESS",
+                    State = "IZMIT",
+                    City = City.Kocaeli.ToString(),
+                    Country = Country.Turkiye.ToString(),
+                    ZipCode = "123456"
+                }
+            };
+            request.SetHashData(envConfig.Password);
+
+            var serviceUrl = envConfig.ServiceUrl;
+            var response = await _ktPayService.PaymentAsync(serviceUrl, request);
+            if (response == null) {
+                
+                return Json(new { Error = "PaymentAsyncError", ErrorMessage = "İşleminiz gerçekleştirilemedi."});
+            }
+            
+            return Content(response, "text/html");
+        }
+        
+        /// <summary>
+        /// 3D Payment - Installment
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IActionResult> InstallmentPayment() {
+            
+            var envConfig = _configuration.GetSection("KTPayConfig:"+KTPayConfig.TEST).Get<KTPayConfig>() ?? new KTPayConfig();
+            var cardConfig = _configuration.GetSection("CardConfig:"+CardConfig.VPOSTest_Installment).Get<CardConfig>() ?? new CardConfig();
+
+            var request = new PaymentRequest() {
+                PaymentType = 1,
+                Language = Language.TR.GetValue(),
+                MerchantOrderId = "KT TEST",
+                SuccessUrl = "http://localhost:3000/KTPay/Success",
+                FailUrl = "http://localhost:3000/KTPay/Fail",
+                MerchantId = Convert.ToInt32(envConfig.MerchantId),
+                CustomerId = Convert.ToInt32(envConfig.CustomerId),
+                Username = envConfig.Username,
+                Amount = "100",
+                Currency = Currency.TRY.ToString(),
+                InstallmentCount = 2,
+                Customer = new Customer() {
+                    FullName = "JOHN DOE",
+                    IdentityNumber = "12345678901",
+                    Email = "noreply@kuveytturk.com.tr",
+                    PhoneNumber = new Phone() {
+                        Cc = "90",
+                        Subscriber = "5001112233"
+                    },
+                    IpAddress = "127.0.0.1"
+                },
+                Card = new Card() {
+                    CardHolderName = cardConfig.CardHolderName,
+                    CardNumber = cardConfig.CardNumber,
+                    ExpireMonth = cardConfig.ExpireMonth,
+                    ExpireYear = cardConfig.ExpireYear,
+                    SecurityCode = cardConfig.SecurityCode 
+                },
+                Cart = new List<CartItem>() {
+                    new CartItem() {
+                        CartItemName = "Product PHYSICAL",
+                        CartItemUrl = "https://www.kuveytturk.com.tr",
+                        CartItemType = (int)CartItemType.PHYSICAL,
+                        Quantity = 1.5,
+                        Price = "10",
+                        TotalAmount = "15"
+                    },
+                    new CartItem() {
+                        CartItemName = "Product VIRTUAL",
+                        CartItemUrl = "https://www.kuveytturk.com.tr",
+                        CartItemType = (int)CartItemType.VIRTUAL,
+                        Quantity = 1,
+                        Price = "85",
+                        TotalAmount = "85"
+                    }
+                },
+                InvoiceAddress = new InvoiceAddress() {
+                    Company = "XXX LTD.STI.",
+                    TaxNumber = "111111111",
+                    TaxOffice = "XXXXXXXXX",
+                    Address = "ADDRESS",
+                    State = "IZMIT",
+                    City = City.Kocaeli.ToString(),
+                    Country = Country.Turkiye.ToString(),
+                    ZipCode = "123456"
+                },
+                ShippingAddress = new ShippingAddress() {
+                    FullName = "JOHN DOE",
+                    Address = "ADDRESS",
+                    State = "IZMIT",
+                    City = City.Kocaeli.ToString(),
+                    Country = Country.Turkiye.ToString(),
+                    ZipCode = "123456"
+                }
+            };
+            request.SetHashData(envConfig.Password);
+
+            var serviceUrl = envConfig.ServiceUrl;
+            var response = await _ktPayService.PaymentAsync(serviceUrl, request);
+            if (response == null) {
+                
+                return Json(new { Error = "InstallmentPaymentAsyncError", ErrorMessage = "İşleminiz gerçekleştirilemedi."});
+            }
+            
+            return Content(response, "text/html");
+        }
+        
+        /// <summary>
+        /// 3D Payment - Callback Url(Success)
+        /// </summary>
+        /// <param name="paymentResponse"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> Success([FromForm]KTPayResponseResult<PaymentResponse> paymentResponse) {
+            
+            var envConfig = _configuration.GetSection("KTPayConfig:"+KTPayConfig.TEST).Get<KTPayConfig>() ?? new KTPayConfig();
+            
+            if (paymentResponse.Success && paymentResponse.ResponseCode == "00") {
+    
+                var request = new ProvisionRequest() {
+                    Language = Language.TR.GetValue(),
+                    MerchantId = Convert.ToInt32(envConfig.MerchantId),
+                    CustomerId = Convert.ToInt32(envConfig.CustomerId),
+                    Username = envConfig.Username,
+                    OrderId = paymentResponse.Result.OrderId,
+                    MerchantOrderId = paymentResponse.Result.MerchantOrderId,
+                    Amount = "100",
+                    Md = paymentResponse.Result.MD
+                };
+                request.SetHashData(envConfig.Password);
+                
+                var serviceUrl = envConfig.ServiceUrl;
+                var provisionResponse = await _ktPayService.ProvisionAsync(serviceUrl, request);
+                if (provisionResponse == null) {
+                    return Json(new { Error = "ProvisionAsyncError", ErrorMessage = "İşleminiz gerçekleştirilemedi."});
+                }
+
+                if (provisionResponse.Success && provisionResponse.ResponseCode == "00") {
+                    
+                    return Json(provisionResponse);
+                }
+    
+                return Json(provisionResponse);
+            }
+
+            return Json(paymentResponse);
+        }
+        
+        /// <summary>
+        /// 3D Payment - Callback Url(Fail)
+        /// </summary>
+        /// <param name="errorVM"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult Fail([FromForm]ErrorVM errorVM) {
+            
+            return Json(errorVM);
+        }
+        
+        /// <summary>
+        /// 3D Payment - Provision
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IActionResult> Provision() {
+            
+            var envConfig = _configuration.GetSection("KTPayConfig:"+KTPayConfig.TEST).Get<KTPayConfig>() ?? new KTPayConfig();
+            
+            var request = new ProvisionRequest() {
+                Language = Language.TR.GetValue(),
+                MerchantId = Convert.ToInt32(envConfig.MerchantId),
+                CustomerId = Convert.ToInt32(envConfig.CustomerId),
+                Username = envConfig.Username,
+                OrderId = 114294527,
+                MerchantOrderId = "2cdfc2a5-4dcb-404e-80b0-126cf743143a",
+                Amount = "100",
+                Md = "9KTajMvFkCEmNHmdwS4NkUrYQjg6l89td4FfGrfFMAfBb61nxxUAbEktO4sCEz2f"
+            };
+            request.SetHashData(envConfig.Password);
+            var serviceUrl = envConfig.ServiceUrl;
+            
+            var provisionAsync = await _ktPayService.ProvisionAsync(serviceUrl, request);
+            if (provisionAsync == null) {
+                return Json(new { Error = "SaleReversalAsyncError", ErrorMessage = "İşleminiz gerçekleştirilemedi."});
+            }
+
+            if (provisionAsync.Success && provisionAsync.ResponseCode == "00") {
+                    
+                return Json(provisionAsync);
+            }
+    
+            return Json(new { Error = provisionAsync.ResponseCode, ErrorMessage = provisionAsync.ResponseMessage, BK = provisionAsync.BusinessKey});
+        }
+
+        /// <summary>
+        /// SaleReversal - Cancel, Drawback, Partial Drawback
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IActionResult> SaleReversal() {
+            
+            var envConfig = _configuration.GetSection("KTPayConfig:"+KTPayConfig.TEST).Get<KTPayConfig>() ?? new KTPayConfig();
+            
+            SaleReversalRequest request = new SaleReversalRequest() {
+                Language = Language.TR.GetValue(),
+                SaleReversalType = SaleReversalType.CANCEL,
+                MerchantId = Convert.ToInt32(envConfig.MerchantId),
+                CustomerId = Convert.ToInt32(envConfig.CustomerId),
+                Username = envConfig.Username,
+                OrderId = 155139799,
+                MerchantOrderId = "KT TEST",
+                Amount = "100"
+            };
+            request.SetHashData(envConfig.Password);
+            
+            var serviceUrl = envConfig.ServiceUrl;
+            var saleReversalAsync = await _ktPayService.SaleReversalAsync(serviceUrl, request);
+            if (saleReversalAsync == null) {
+                return Json(new { Error = "SaleReversalAsyncError", ErrorMessage = "İşleminiz gerçekleştirilemedi."});
+            }
+
+            if (saleReversalAsync.Success && saleReversalAsync.ResponseCode == "00") {
+                    
+                return Json(saleReversalAsync);
+            }
+    
+            return Json(new { Error = saleReversalAsync.ResponseCode, ErrorMessage = saleReversalAsync.ResponseMessage, BusinessKey = saleReversalAsync.BusinessKey});
+        }
+        
+        /// <summary>
+        /// GetTransaction
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IActionResult> GetTransaction() {
+            
+            var envConfig = _configuration.GetSection("KTPayConfig:"+KTPayConfig.TEST).Get<KTPayConfig>() ?? new KTPayConfig();
+            
+            GetTransactionRequest request = new GetTransactionRequest() {
+                Language = Language.TR.GetValue(),
+                MerchantId = Convert.ToInt32(envConfig.MerchantId),
+                CustomerId = Convert.ToInt32(envConfig.CustomerId),
+                Username = envConfig.Username,
+                OrderId = 155139799
+            };
+            request.SetHashData(envConfig.Password);
+            var serviceUrl = envConfig.ServiceUrl;
+            
+            var getTransactionAsync = await _ktPayService.GetTransactionAsync(serviceUrl, request);
+            if (getTransactionAsync == null) {
+                return Json(new { Error = "getTransactionAsyncError", ErrorMessage = "İşleminiz gerçekleştirilemedi."});
+            }
+
+            if (getTransactionAsync.Success) {
+                    
+                return Json(getTransactionAsync);
+            }
+    
+            return Json(new { Error = getTransactionAsync.ResponseCode, ErrorMessage = getTransactionAsync.ResponseMessage, BusinessKey = getTransactionAsync.BusinessKey});
+        }
+        
+        /// <summary>
+        /// GetTransactions
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IActionResult> GetTransactions() {
+            
+            var envConfig = _configuration.GetSection("KTPayConfig:"+KTPayConfig.TEST).Get<KTPayConfig>() ?? new KTPayConfig();
+
+            GetTransactionsRequest request = new GetTransactionsRequest() {
+                Language = Language.TR.GetValue(),
+                MerchantId = Convert.ToInt32(envConfig.MerchantId),
+                CustomerId = Convert.ToInt32(envConfig.CustomerId),
+                Username = envConfig.Username,
+                MerchantOrderId = "KT TEST"
+            };
+            request.SetHashData(envConfig.Password);
+            var serviceUrl = envConfig.ServiceUrl;
+            
+            var getTransactionsAsync = await _ktPayService.GetTransactionsAsync(serviceUrl, request);
+            if (getTransactionsAsync == null) {
+                return Json(new { Error = "getTransactionAsyncError", ErrorMessage = "İşleminiz gerçekleştirilemedi."});
+            }
+
+            if (getTransactionsAsync.Success) {
+                    
+                return Json(getTransactionsAsync);
+            }
+    
+            return Json(new { Error = getTransactionsAsync.ResponseCode, ErrorMessage = getTransactionsAsync.ResponseMessage, BusinessKey = getTransactionsAsync.BusinessKey});
+        }
+    }
+}
---
+++ KTPay/Models/City.cs
@@ -0,0 +1,102 @@
+﻿namespace KTPay.Models {
+    
+    public sealed class City {
+    
+        // City ISO 3166-1 Alpha-2 Codes
+        // Ref: https://www.iso.org/obp/ui/#iso:code:3166:TR
+        
+        private readonly string _value;
+
+        public static readonly City Adana               = new City("01");
+        public static readonly City Adiyaman            = new City("02");
+        public static readonly City Afyonkarahisar      = new City("03");
+        public static readonly City Agri                = new City("04");
+        public static readonly City Aksaray             = new City("68");
+        public static readonly City Amasya              = new City("05");
+        public static readonly City Ankara              = new City("06");
+        public static readonly City Antalya             = new City("07");
+        public static readonly City Ardahan             = new City("75");
+        public static readonly City Artvin              = new City("08");
+        public static readonly City Aydin               = new City("09");
+        public static readonly City Balikesir           = new City("10");
+        public static readonly City Bartin              = new City("74");
+        public static readonly City Batman              = new City("72");
+        public static readonly City Bayburt             = new City("69");
+        public static readonly City Bilecik             = new City("11");
+        public static readonly City Bingol              = new City("12");
+        public static readonly City Bitlis              = new City("13");
+        public static readonly City Bolu                = new City("14");
+        public static readonly City Burdur              = new City("15");
+        public static readonly City Bursa               = new City("16");
+        public static readonly City Canakkale           = new City("17");
+        public static readonly City Cankiri             = new City("18");
+        public static readonly City Corum               = new City("19");
+        public static readonly City Denizli             = new City("20");
+        public static readonly City Diyarbakir          = new City("21");
+        public static readonly City Duzce               = new City("81");
+        public static readonly City Edirne              = new City("22");
+        public static readonly City Elazig              = new City("23");
+        public static readonly City Erzincan            = new City("24");
+        public static readonly City Erzurum             = new City("25");
+        public static readonly City Eskisehir           = new City("26");
+        public static readonly City Gaziantep           = new City("27");
+        public static readonly City Giresun             = new City("28");
+        public static readonly City Gumushane           = new City("29");
+        public static readonly City Hakkari             = new City("30");
+        public static readonly City Hatay               = new City("31");
+        public static readonly City Igdir               = new City("76");
+        public static readonly City Isparta             = new City("32");
+        public static readonly City Istanbul            = new City("34");
+        public static readonly City Izmir               = new City("35");
+        public static readonly City Kahramanmaras       = new City("46");
+        public static readonly City Karabuk             = new City("78");
+        public static readonly City Karaman             = new City("70");
+        public static readonly City Kars                = new City("36");
+        public static readonly City Kastamonu           = new City("37");
+        public static readonly City Kayseri             = new City("38");
+        public static readonly City Kirikkale           = new City("71");
+        public static readonly City Kirklareli          = new City("39");
+        public static readonly City Kirsehir            = new City("40");
+        public static readonly City Kilis               = new City("79");
+        public static readonly City Kocaeli             = new City("41");
+        public static readonly City Konya               = new City("42");
+        public static readonly City Kutahya             = new City("43");
+        public static readonly City Malatya             = new City("44");
+        public static readonly City Manisa              = new City("45");
+        public static readonly City Mardin              = new City("47");
+        public static readonly City Mersin              = new City("33");
+        public static readonly City Mugla               = new City("48");
+        public static readonly City Mus                 = new City("49");
+        public static readonly City Nevsehir            = new City("50");
+        public static readonly City Nigde               = new City("51");
+        public static readonly City Ordu                = new City("52");
+        public static readonly City Osmaniye            = new City("80");
+        public static readonly City Rize                = new City("53");
+        public static readonly City Sakarya             = new City("54");
+        public static readonly City Samsun              = new City("55");
+        public static readonly City Siirt               = new City("56");
+        public static readonly City Sinop               = new City("57");
+        public static readonly City Sivas               = new City("58");
+        public static readonly City Sanliurfa           = new City("63");
+        public static readonly City Sirnak              = new City("73");
+        public static readonly City Tekirdag            = new City("59");
+        public static readonly City Tokat               = new City("60");
+        public static readonly City Trabzon             = new City("61");
+        public static readonly City Tunceli             = new City("62");
+        public static readonly City Usak                = new City("64");
+        public static readonly City Van                 = new City("65");
+        public static readonly City Yalova              = new City("77");
+        public static readonly City Yozgat              = new City("66");
+        public static readonly City Zonguldak           = new City("67");
+
+        private City(string value) {
+            
+            _value = value;
+        }
+
+        public override string ToString() {
+            
+            return _value;
+        }
+    }
+}
